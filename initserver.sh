7b33cb78e596dd16349e7adde24581fe56598d95
Daniel <daniel@scholllopez.com>
2017-08-11 18:47:00 -0500

    Added TMux, VIM and nodemoudles

---
+++ initserver.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates software-properties-common
+sudo apt-get install -y curl
+sudo apt-get install -y libssl-dev libffi-dev build-essentials
+sudo apt-get install -y python-dev python-pip
+sudo apt-get install -y ansible
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
--- nodemodules.sh
+++ nodemodules.sh
@@ -1,14 +1,12 @@
-npm install -g npm@latest
-npm install -g yarn
-yarn global add yo
-yarn global add grunt-cli
-yarn global add bower
-yarn global add gulp
-yarn global add less
-yarn global add mocha
-yarn global add node-static
-yarn global add nodemon
-yarn global add azure-cli
-yarn global add protractor
-yarn global add karma-cli
-yarn global add avn avn-nvm avn-n
+sudo npm install -g npm@latest
+sudo npm install -g yarn
+sudo yarn global add yo
+sudo yarn global add mocha
+sudo yarn global add tape
+sudo yarn global add node-static
+sudo yarn global add nodemon
+sudo yarn global add azure-cli
+sudo yarn global add protractor
+sudo yarn global add karma-cli
+sudo yarn global add eslint
+sudo yarn global add avn avn-nvm avn-n
