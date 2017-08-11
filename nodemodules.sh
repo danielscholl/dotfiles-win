1fbe7154ab3129214989cec1cbc1e63636913244
Daniel <daniel@scholllopez.com>
2017-08-11 17:17:57 -0500

    Initial Code

---
+++ nodemodules.sh
@@ -0,0 +1,14 @@
+npm install -g npm@latest
+npm install -g yarn
+yarn global add yo
+yarn global add grunt-cli
+yarn global add bower
+yarn global add gulp
+yarn global add less
+yarn global add mocha
+yarn global add node-static
+yarn global add nodemon
+yarn global add azure-cli
+yarn global add protractor
+yarn global add karma-cli
+yarn global add avn avn-nvm avn-n
