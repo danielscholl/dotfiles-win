0eb2acceb899ca4099ed95dec2800244567148b2
Daniel <daniel@scholllopez.com>
2017-08-12 07:18:39 -0500

    Added Azure CLI

--- initserver.sh
+++ initserver.sh
@@ -1,8 +1,9 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates software-properties-common
 sudo apt-get install -y curl
-sudo apt-get install -y libssl-dev libffi-dev build-essentials
+sudo apt-get install -y libssl-dev libffi-dev build-essential
 sudo apt-get install -y python-dev python-pip
 sudo apt-get install -y ansible
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+curl -L https://aka.ms/InstallAzureCli | bash
