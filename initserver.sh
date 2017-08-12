1b0cb390c96523452359e4f9301037b37f9c69f1
Daniel <daniel@scholllopez.com>
2017-08-12 08:09:34 -0500

    Rework Node

--- initserver.sh
+++ initserver.sh
@@ -1,8 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates software-properties-common
-sudo apt-get install -y curl
+sudo apt-get install -y apt-transport-https ca-certificates software-properties-common curl
 sudo apt-get update && apt-get install -y libssl-dev libffi-dev python-dev build-essential
 sudo apt-get install -y ansible
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
 curl -L https://aka.ms/InstallAzureCli | bash
--- nodemodules.sh
+++ nodemodules.sh
@@ -1,12 +1,16 @@
-sudo npm install -g npm@latest
-sudo npm install -g yarn
-sudo yarn global add yo
-sudo yarn global add mocha
-sudo yarn global add tape
-sudo yarn global add node-static
-sudo yarn global add nodemon
-sudo yarn global add azure-cli
-sudo yarn global add protractor
-sudo yarn global add karma-cli
-sudo yarn global add eslint
-sudo yarn global add avn avn-nvm avn-n
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source "$HOME/.nvm/nvm.sh"
+nvm install lts/*
+nvm use lts/*
+npm install -g npm@latest
+npm install -g yarn
+yarn global add yo
+yarn global add mocha
+yarn global add tape
+yarn global add node-static
+yarn global add nodemon
+yarn global add azure-cli
+yarn global add protractor
+yarn global add karma-cli
+yarn global add eslint
+yarn global add avn avn-nvm avn-n
