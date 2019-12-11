3e578541f2baa5131a207e069ace99cb15e53e8a
Daniel Scholl <daniel.scholl@microsoft.com>
2019-12-11 12:37:37 -0600

    Updated dotfiles

--- initserver.sh
+++ initserver.sh
@@ -1,18 +1,44 @@
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates software-properties-common curl
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get install -y apt-transport-https ca-certificates software-properties-common curl wget
 sudo apt-get update && apt-get install -y libssl-dev libffi-dev python-dev build-essential
-
-# Install Node Version Manager (N)
-#PREFIX="$HOME/.n"
-#PATH="$HOME/$N_PREFIX/bin:$HOME/bin:$HOME/.local/bin:/usr/local/sbin:/usr/local/bin/user/sbin:/usr/bin:/sbin:/bin"
-#curl -L https://git.io/n-install | bash
-
-
-# Install Node Version Manager (NVM)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-
-# Install Ansible
 sudo apt-get install -y ansible
 
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ${USER}
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
 # Install Azure CLI
-curl -L https://aka.ms/InstallAzureCli | bash
+ln -s /usr/bin/python3 /usr/bin/python
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+CLI_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ ${CLI_REPO} main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install -y azure-cli
+
+
+# Install Dotnet Core
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" | sudo tee /etc/apt/sources.list.d/dotnetdev.list
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-2.2
+
+
+# Install Powershell Core
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
+sudo apt -y install powershell
+
+
+# Install Homebrew
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+# test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# test -r ~/.bashrc && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
